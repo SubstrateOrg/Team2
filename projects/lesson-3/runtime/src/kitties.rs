cde3424203aff0b8c8a89d59e13a4686eaf1be4a
antilopes <631640592@qq.com>
2019-09-22 08:21:26 -0700

    breed kitty

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -33,5 +33,36 @@ decl_module! {
 			Kitties::insert(count, kitty);
 			KittiesCount::put(count + 1);
 		}
+
+		/// breed a child kitty
+		pub fn breed(origin, index1: u32, index2: u32) {
+			let sender = ensure_signed(origin)?;
+			
+			if index1 == index2 || index1 == u32::max_value() || index2 == u32::max_value() {
+				return Err("Invalid parent kitties ID");
+			}
+			let kitty1 = Self::kitty(index1);
+			let kitty2 = Self::kitty(index2);
+			
+			let count = Self::kitties_count();
+			if count == u32::max_value() {
+				return Err("Kitties count overflow");
+			}
+
+			//calc child kitty dna
+			let kitty1_dna = kitty1.0;
+			let kitty2_dna = kitty2.0;
+			let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+			let dna = payload.using_encoded(blake2_128);
+			let mut child_dna = [0u8; 16];
+			for i in 0..kitty1_dna.len() {
+				child_dna[i] = (!dna[i] & kitty1_dna[i]) | (dna[i] & kitty2_dna[i]);
+			}
+
+			let child_kitty = Kitty(child_dna);
+			Kitties::insert(count, child_kitty);
+			KittiesCount::put(count + 1);
+		}
 	}
+
 }

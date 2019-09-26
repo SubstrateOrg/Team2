f48cccf35c0f0f6c3a0969b73c5f9298837b928f
Edward Shi <lancelot96@protonmail.ch>
2019-09-26 09:33:45 +0800

    lesson 4

--- projects/lesson-4/runtime/src/kitties.rs
+++ projects/lesson-4/runtime/src/kitties.rs
@@ -32,26 +32,7 @@ decl_module! {
 		pub fn create(origin) {
 			let sender = ensure_signed(origin)?;
 
-			// 作业：重构create方法，避免重复代码
-
-			let kitty_id = Self::kitties_count();
-			if kitty_id == T::KittyIndex::max_value() {
-				return Err("Kitties count overflow");
-			}
-
-			// Generate a random 128bit value
-			let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
-			let dna = payload.using_encoded(blake2_128);
-
-			// Create and store kitty
-			let kitty = Kitty(dna);
-			<Kitties<T>>::insert(kitty_id, kitty);
-			<KittiesCount<T>>::put(kitty_id + 1.into());
-
-			// Store the ownership information
-			let user_kitties_id = Self::owned_kitties_count(&sender);
-			<OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
-			<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
+			Self::do_create(sender)?;
 		}
 
 		/// Breed kitties
@@ -60,16 +41,20 @@ decl_module! {
 
 			Self::do_breed(sender, kitty_id_1, kitty_id_2)?;
 		}
+
+		pub fn transfer(origin, kitty_id: T::KittyIndex, to: T::AccountId) {
+			let sender = ensure_signed(origin)?;
+
+			Self::do_transfer(sender, kitty_id, to)?;
+		}
 	}
 }
 
 fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
-	// 作业：实现combine_dna
-	// 伪代码：
-	// selector.map_bits(|bit, index| if (bit == 1) { dna1 & (1 << index) } else { dna2 & (1 << index) })
-	// 注意 map_bits这个方法不存在。只要能达到同样效果，不局限算法
-	// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
-	return dna1;
+	let dna1 = selector & dna1;
+	let dna2 = !selector & dna2;
+
+	dna1 | dna2
 }
 
 impl<T: Trait> Module<T> {
@@ -95,6 +80,18 @@ impl<T: Trait> Module<T> {
 		let user_kitties_id = Self::owned_kitties_count(owner.clone());
 		<OwnedKitties<T>>::insert((owner.clone(), user_kitties_id), kitty_id);
 		<OwnedKittiesCount<T>>::insert(owner, user_kitties_id + 1.into());
+	}
+
+	fn do_create(sender: T::AccountId) -> Result {
+		let kitty_id = Self::next_kitty_id()?;
+
+		// Generate a random 128bit value
+		let dna = Self::random_value(&sender);
+
+		// Create and store kitty
+		Self::insert_kitty(sender, kitty_id, Kitty(dna));
+
+		Ok(())
 	}
 
 	fn do_breed(sender: T::AccountId, kitty_id_1: T::KittyIndex, kitty_id_2: T::KittyIndex) -> Result {
@@ -123,4 +120,34 @@ impl<T: Trait> Module<T> {
 
 		Ok(())
 	}
+
+	fn do_transfer(sender: T::AccountId, kitty_id: T::KittyIndex, to: T::AccountId) -> Result {
+		let kitty_counts = Self::owned_kitties_count(&sender);
+		ensure!(kitty_id < kitty_counts, "Invalid kitty_id");
+		let global_kitty_id = Self::owned_kitties((sender.clone(), kitty_id));
+
+		let last_kitty_id = kitty_counts - 1.into();
+		if last_kitty_id != kitty_id {
+			let last_global_kitty_id =  Self::owned_kitties((sender.clone(), last_kitty_id));
+			<OwnedKitties<T>>::remove((sender.clone(), last_kitty_id));
+			<OwnedKitties<T>>::insert((sender.clone(), kitty_id), last_global_kitty_id);
+		}
+
+		<OwnedKittiesCount<T>>::insert(sender, last_kitty_id);
+
+		let last_kitty_id = Self::owned_kitties_count(&to);
+		<OwnedKitties<T>>::insert((to.clone(), last_kitty_id), global_kitty_id);
+		<OwnedKittiesCount<T>>::insert(to, last_kitty_id + 1.into());
+
+		Ok(())
+	}
+}
+
+#[test]
+fn test_combine_dna() {
+	let dna1 = 0b11110000;
+	let dna2 = 0b11001100;
+	let selector = 0b10101010;
+
+	assert_eq!(combine_dna(dna1, dna2, selector), 0b11100100);
 }

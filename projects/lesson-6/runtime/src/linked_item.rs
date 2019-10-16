f0b8e8129fd3203e6c28701a1e3005942285ff10
fanyanjun <fyj_beyond@126.com>
2019-10-16 17:01:27 +0800

    Update linked_item.rs

--- projects/lesson-6/runtime/src/linked_item.rs
+++ projects/lesson-6/runtime/src/linked_item.rs
@@ -37,9 +37,46 @@ impl<Storage, Key, Value> LinkedList<Storage, Key, Value> where
 
     pub fn append(key: &Key, value: Value) {
         // 作业：实现 append
+		let head = Self::read_head(key);
+		let new_head = LinkedItem {
+ 			prev: Some(value),
+ 			next: head.next,
+ 		};
+
+		Self::write_head(key, new_head);
+
+		let prev = Self::read(key, head.prev);
+		let new_prev = LinkedItem {
+ 			prev: prev.prev,
+ 			next: Some(value),
+ 		};
+		Self::write(key, head.prev, new_prev);
+
+		let item = LinkedItem {
+ 			prev: head.prev,
+ 			next: None,
+ 		};
+ 		Self::write(key, Some(value), item);
     }
 
     pub fn remove(key: &Key, value: Value) {
         // 作业：实现 remove
+		if let Some(item) = Storage::take(&(key.clone(), Some(value))) {
+			let prev = Self::read(key, item.prev);
+			let new_prev = LinkedItem {
+ 				prev: prev.prev,
+ 				next: item.next,
+ 			};
+
+			Self::write(key, item.prev, new_prev);
+
+			let next = Self::read(key, item.next);
+ 			let new_next = LinkedItem {
+ 				prev: item.prev,
+ 				next: next.next,
+ 			};
+
+  			Self::write(key, item.next, new_next);
+		}
     }
 }
